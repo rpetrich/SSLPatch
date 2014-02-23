643dbf530fe583f6536f57c96293eaf5fd3e17e8
Ryan Petrich <rpetrich@gmail.com>
2014-02-23 13:45:21 -0700

    Insert dummy replacement for ccec_full_ctx_decl

--- corecrypto/ccec.h
+++ corecrypto/ccec.h
@@ -1,4 +1,7 @@
 #import <corecrypto/ccn.h>
 
-#define ccec_full_ctx_decl(size, name) char name[size]
+#define ccec_full_ctx_decl(size, name) \
+	long name ## _1; \
+	long name ## _2; \
+	char name ## _3[292]
 
